84e3b9416d0260140ba441eb5de74656d172f433
fubira <fubira@gmail.com>
2019-08-01 19:44:07 +0900

    test data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,58 @@
 <template>
-  <v-container>
-    <v-layout text-center wrap>
-      <v-flex mb-4>
-        <v-card  max-width="800">
-          <v-card-text>現在時刻</v-card-text>
-          <v-card-text>12:34</v-card-text>
-        </v-card>
-      </v-flex>
+  <v-container fluid>
+    <v-layout column>
+      <v-card class="item">
+        <v-flex xsl2>
+          <div class="resultContainer">
+            <v-layout align-center justify-space-around row fill-height>
+              <div style="width: 10%" class="display-1">先発</div>
+              <div class="display-3">12:34</div>
+              <div style="width: 40%" class="display-2">ハフリンコ <span class="display-1">行</span></div>
+              <div class="display-1">1番</div>
+            </v-layout>
+          </div>
+        </v-flex>
+      </v-card>
+
+      <v-card class="item">
+        <v-flex xsl2>
+          <div class="resultContainer">
+            <v-layout align-center justify-space-around row fill-height>
+              <div style="width: 10%" class="display-1">次発</div>
+              <div class="display-3">12:55</div>
+              <div style="width: 40%" class="display-2">名古屋 <span class="display-1">行</span></div>
+              <div class="display-1">2番</div>
+            </v-layout>
+          </div>
+        </v-flex>
+      </v-card>
+
+      <v-card class="item">
+        <v-flex xsl2>
+          <div class="resultContainer">
+            <v-layout align-center justify-space-around row fill-height>
+              <div style="width: 10%" class="display-1"></div>
+              <div class="display-3">13:10</div>
+              <div style="width: 40%" class="display-2">東京 <span class="display-1">行</span></div>
+              <div class="display-1">3番</div>
+            </v-layout>
+          </div>
+        </v-flex>
+      </v-card>
+
+      <v-card class="item">
+        <v-flex xsl2>
+          <div class="resultContainer">
+            <v-layout align-center justify-space-around row fill-height>
+              <div style="width: 10%" class="display-1"></div>
+              <div class="display-3">13:22</div>
+              <div style="width: 40%" class="display-2">大阪 <span class="display-1">行</span></div>
+              <div class="display-1">2番</div>
+            </v-layout>
+          </div>
+        </v-flex>
+      </v-card>
+
     </v-layout>
   </v-container>
 </template>
@@ -17,3 +63,11 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss">
+
+.item {
+  padding: 20px;
+  margin: 5px;
+}
+</style>
